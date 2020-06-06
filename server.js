fe3208c08a0c0c848c6ad6808f8fbde177116405
sujatha <jhanavi.bhushan@gmail.com>
2020-06-06 14:34:41 -0400

    Implemented server.js with mongoose set up

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+// const db = require('./models')
+
+// Open the database connection
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/custommethoddb',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    }
+  )
+
+// Create the Express app
+const app = express()
+
+// Assign Express global middleware
+app.use(logger('dev'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+// Start listening for HTTP requests
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`))
